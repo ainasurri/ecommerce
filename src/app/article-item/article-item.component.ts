03ec3418bbe10ca5e264ff5a68284783c0dd849e
AinaSorribas <122831540+AinaSorribas@users.noreply.github.com>
2023-12-08 17:51:23 +0100

    ex3

--- src/app/article-item/article-item.component.ts
+++ src/app/article-item/article-item.component.ts
@@ -10,7 +10,18 @@ import { Component } from '@angular/core';
 export class ArticleItemComponent {
   name: string = 'Tassa verda';
   price: number = 3.99;
-  imageUrl: string = 'imatge.jpg'; 
+  imageUrl: string = "../../assets/tassa.png";
   isOnSale: boolean = false;
   quantityInCart: number = 0;
+
+  increment() {
+    this.quantityInCart++;
+  }
+
+  decrement() {
+    if (this.quantityInCart > 0) {
+      this.quantityInCart--;
+    }
+  }
+
 }
